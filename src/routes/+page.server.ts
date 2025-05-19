a3e6ad99ecbc93e88c758476c4b0c4f7453565d1
Julius Polar <julius.polar@protonmail.com>
2025-05-19 13:20:16 +0200

    try vercel

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,18 @@
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ fetch, request }) => {
+  const headers = request.headers;
+  const city = headers.get("x-vercel-ip-city") || "München";
+
+  const citySearch = await fetch(`
+    https://geocoding-api.open-meteo.com/v1/search?count=10&language=en&format=json&name=${encodeURIComponent(city)}
+  `).then((res) => res.json());
+
+  if (citySearch.results) {
+    const result = citySearch.results[0];
+
+    return await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${result.latitude}&longitude=${result.longitude}&daily=weather_code&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code&timezone=auto`
+    ).then((res) => res.json());
+  }
+};
--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = async ({ fetch }) => {
-  return await fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=48.1374&longitude=11.5755&daily=weather_code&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code&timezone=auto"
-  ).then((res) => res.json());
-};
