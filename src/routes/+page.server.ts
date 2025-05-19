267d2722bc1f14debe8334613e36cf0d468666c9
Julius Polar <julius.polar@protonmail.com>
2025-05-19 14:00:13 +0200

    remove encode uri

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,15 @@
-import { json } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ fetch, request }) => {
   const headers = request.headers;
   const city = headers.get("X-Vercel-IP-City");
 
-  return { city };
-
   const citySearch = await fetch(`
-    https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name=${encodeURIComponent(city)}
+    https://geocoding-api.open-meteo.com/v1/search?count=1&language=en&format=json&name=${city}
   `).then((res) => res.json());
 
   if (citySearch.results) {
     const result = citySearch.results[0];
-
-    console.log(citySearch.results[0]);
 
     return await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${result.latitude}&longitude=${result.longitude}&daily=weather_code&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code&timezone=auto`
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,10 @@
   import type { WeatherData } from "$lib/weather.types";
   import type { PageProps } from "./$types";
 
-  //const { data: weatherData }: PageProps = $props();
-  //const data = weatherData as WeatherData;
-  const { data }: PageProps = $props();
+  const { data: weatherData }: PageProps = $props();
+  const data = weatherData as WeatherData;
 </script>
 
 <div class="mx-auto flex min-h-full w-full max-w-xl flex-col space-y-1 sm:pt-4">
-  <p>{data.city}</p>
+  <Main {data} />
 </div>
